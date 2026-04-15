7ae85b14be5d917b035c8c13b760fb82a1df5bb7
khins <kevin@hinshawfam.com>
2026-04-15 10:29:31 -0500

    add double value function

--- lesson14_references.cpp
+++ lesson14_references.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std; // This program demonstrates the difference between pass by value and pass by reference in C++.
 
+void doubleValue(int &x) {
+    x = x * 2; // This modifies the local copy of x, not the original variable passed in
+}
+
 // Pass by value (copy)
 // funtion receives a copy of the argument, so changes to x do not affect num in main and
 // does not reurn any value to the caller
@@ -27,6 +31,9 @@ int main() {
     // Pass by reference: num is passed as a reference, so changes in changeReference affect num in main
     changeReference(num);
     cout << "After changeReference: " << num << endl;
+
+    doubleValue(num);
+    cout << "After doubleValue: " << num << endl; // num remains unchanged because doubleValue uses pass by value
 
     cout << endl;
 
